92eeee0d0cf9fb2e287c5dde6eb71f506440ee72
erasstranica <erasstranica@gmail.com>
2019-01-16 21:22:58 +0100

    update

--- main.js
+++ main.js
@@ -2,8 +2,24 @@ var edu = 0;
 var hea = 0;
 var con = 0;
 function myfunc(){
-$('input:radio').each(function() {
+$('input').each(function() {
     
+  if($(this).is(':checked')) {
+      alert($(this));
+    if($(this).val() === 'education'){
+        edu = edu + 1;
+    }
+    else if ($(this).val() === 'health'){
+        hea = hea + 1;
+    }
+    else{
+        con = con + 1;
+    }
+  }   
+});
+
+/*$('input:checkbox').each(function() {
+    alert($(this));
   if($(this).is(':checked')) {
       //alert($(this).val());
     if($(this).val() === 'education'){
@@ -17,7 +33,7 @@ $('input:radio').each(function() {
     }
   }   
 });
-
+*/
 if(edu >= hea && edu >= con){
     window.open('./education.html', '_self');
 }
